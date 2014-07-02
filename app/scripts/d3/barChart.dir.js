95515f6bbcb5c2c46dc132f0b5818f830f34d851
Russell Wenban <russell@factornine.co.uk>
2014-07-02 14:23:10 +0100

    #3: Website - Skills Matrix - Completed

--- app/scripts/d3/barChart.dir.js
+++ app/scripts/d3/barChart.dir.js
@@ -1,93 +1,100 @@
 'use strict';
 
-angular.module('f9App').directive('d3Bars', ['$window', '$timeout', 'd3Service', 
-  function($window, $timeout, d3Service) {
-    return {
-      restrict: 'A',
-      scope: {
-        data: '=',
-        label: '@',
-        onClick: '&'
-      },
-      link: function(scope, ele, attrs) {
-        d3Service.d3().then(function(d3) {
+angular.module('f9App').directive('d3Bars', ['$window', '$timeout', 'd3Service',
+	function ($window, $timeout, d3Service) {
+		return {
+			restrict: 'EA',
+			scope: {
+				data: '=data',
+				colors: '=colors'
+			},
+			link: function (scope, element, attrs) {
+				d3Service.d3().then(function (d3) {
+					var margin = parseInt(attrs.margin) || 20,
+						barHeight = parseInt(attrs.barHeight) || 20,
+						barPadding = parseInt(attrs.barPadding) || 5;
 
-          var renderTimeout;
-          var margin = parseInt(attrs.margin) || 20,
-              barHeight = parseInt(attrs.barHeight) || 20,
-              barPadding = parseInt(attrs.barPadding) || 5;
+					var svg = d3.select(element[0])
+						.append('svg')
+						.style('width', '100%');
 
-          var svg = d3.select(ele[0])
-            .append('svg')
-            .style('width', '100%');
+					// Browser onresize event
+					window.onresize = function () {
+						scope.$apply();
+					};
 
-          $window.onresize = function() {
-            scope.$apply();
-          };
+					// hard-code data
+//					scope.data = [
+//						{name: "AS3", score: 4},
+//						{name: "JavaScript", score: 3},
+//						{name: 'HTML5', score: 3},
+//						{name: "CSS", score: 2}
+//					];
 
-          scope.$watch(function() {
-            return angular.element($window)[0].innerWidth;
-          }, function() {
-            scope.render(scope.data);
-          });
+					// Watch for resize event
+					scope.$watch(function () {
+						return angular.element($window)[0].innerWidth;
+					}, function () {
+						scope.render(scope.data);
+					});
 
-          scope.$watch('data', function(newData) {
-            scope.render(newData);
-          }, true);
+					scope.render = function (data) {
+						// remove all previous items before render
+						svg.selectAll('*').remove();
 
-          scope.render = function(data) {
-            svg.selectAll('*').remove();
+						// If we don't pass any data, return out of the element
+						if (!data) {
+							return;
+						}
 
-            if (!data) return;
-            if (renderTimeout) clearTimeout(renderTimeout);
+						// setup variables
+						var width = d3.select(element[0]).node().offsetWidth - margin,
+						// calculate the height
+							height = scope.data.length * (barHeight + barPadding),
+						// Use the category20() scale function for multicolor support
+						//	color = d3.scale.category20(),
+						// our xScale
+							xScale = d3.scale.linear()
+								.domain([0, d3.max(data, function (d) {
+									return d.score;
+								})])
+								.range([0, width]);
 
-            renderTimeout = $timeout(function() {
-              var width = d3.select(ele[0])[0][0].offsetWidth - margin,
-                  height = scope.data.length * (barHeight + barPadding),
-                  color = d3.scale.category20(),
-                  xScale = d3.scale.linear()
-                    .domain([0, d3.max(data, function(d) {
-                      return d.score;
-                    })])
-                    .range([0, width]);
+						// set the height based on the calculations above
+						svg.attr('height', height);
 
-              svg.attr('height', height);
-
-              svg.selectAll('rect')
-                .data(data)
-                .enter()
-                  .append('rect')
-                  .on('click', function(d,i) {
-                    return scope.onClick({item: d});
-                  })
-                  .attr('height', barHeight)
-                  .attr('width', 140)
-                  .attr('x', Math.round(margin/2))
-                  .attr('y', function(d,i) {
-                    return i * (barHeight + barPadding);
-                  })
-                  .attr('fill', function(d) {
-                    return color(d.score);
-                  })
-                  .transition()
-                    .duration(1000)
-                    .attr('width', function(d) {
-                      return xScale(d.score);
-                    });
-              svg.selectAll('text')
-                .data(data)
-                .enter()
-                  .append('text')
-                  .attr('fill', '#fff')
-                  .attr('y', function(d,i) {
-                    return i * (barHeight + barPadding) + 15;
-                  })
-                  .attr('x', 15)
-                  .text(function(d) {
-                    return d.name + " (scored: " + d.score + ")";
-                  });
-            }, 200);
-          };
-        });
-      }}
-}])
+						svg.selectAll('rect')
+							.data(data).enter()
+							.append('rect')
+							.attr('height', barHeight)
+							.attr('width', 140)
+							.attr('x', Math.round(margin / 2))
+							.attr('y', function (d, i) {
+								return i * (barHeight + barPadding);
+							})
+							.attr('fill', function (d) {
+								return scope.colors[d.color - 1];
+							})
+							.transition()
+							.duration(1000)
+							.attr('width', function (d) {
+								return xScale(d.score);
+							});
+						svg.selectAll('text')
+							.data(data)
+							.enter()
+							.append('text')
+							.attr('fill', '#AAAAAAA')
+							.attr('y', function (d, i) {
+								return i * (barHeight + barPadding) + 20;
+							})
+							.attr('x', 25)
+							.attr('height', 36)
+							.text(function (d) {
+								return d.name + ' - ' + d.score;
+							});
+					};
+				});
+			}
+		};
+	}]);
--- app/scripts/skills/skills.ctrl.js
+++ app/scripts/skills/skills.ctrl.js
@@ -3,10 +3,37 @@
 angular.module('f9App').controller('SkillsCtrl', ['$log', '$scope',
 	function ($log, $scope) {
 
-		$scope.data='1,4,2,2,5,4,4,2';
+		/**
+		 * 1 Programming Language
+		 * 2 Framework
+		 * 3 Subject Area
+		 * 4 Tool
+		 * 5 Design / Markup - was #6E003E | 8E 10 5E
+		 *
+		 */
+		$scope.colors = ['#ff7f0e','#3CB03C','#1f77b4','#2F87C4','#8E205E'];
 
-		$scope.skills='AS3,UNIX,GIT,JavaScript,Angular.js,Grunt,Yeoman,iOS Developemnt,';
-
-		$scope.newData = JSON.stringify({ 'AS3':4, 'GIT':2 });
-
+		$scope.data = [
+			{name: 'Angular.js', score: 4, color:2},
+			{name: 'JavaScript', score: 3, color:1},
+			{name: 'GIT', score: 3, color:4},
+			{name: 'Grunt', score: 3, color:4},
+			{name: 'Yeoman', score: 2, color:4},
+			{name: 'Twitter Bootstrap', score: 3, color:5},
+			{name: 'CSS3', score: 2, color:5},
+			{name: 'd3', score: 1, color:5},
+			{name: 'UNIX', score: 2,  color:1},
+			{name: 'SAAS', score: 1, color:5},
+			{name: 'UI/UX', score: 4, color:3},
+			{name: 'E-learning', score: 5, color:3},
+			{name: 'iOS', score: 1, color:3},
+			{name: 'Objective-C', score: 1, color:1},
+			{name: 'Flex', score: 2, color:1},
+			{name: 'AS3', score: 5, color:1},
+			{name: 'ANT', score: 2, color:4},
+			{name: 'HTML5', score: 3, color:5},
+			{name: 'MVC', score: 4, color:2},
+			{name: 'Design Patterns', score: 3, color:2},
+			{name: 'SVN', score: 3, color:4}
+		];
 	}]);
