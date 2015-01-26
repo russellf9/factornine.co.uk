d83c60b450d475bccf05b6bb51d6ac8a37a21e6e
russellf9 <russell@factornine.co.uk>
2015-01-26 15:25:29 +0000

    * Re-factoring  - The date module has been implemented.

---
+++ app/scripts/date/date.js
@@ -0,0 +1,22 @@
+'use strict';
+'use strict';
+
+var date = angular.module('date', []);
+
+date.controller('DateController', ['$log', '$scope', function($log, $scope) {
+    //$log.info('A DateCtrl: modified: ',document.lastModified);
+
+    var date = document.lastModified.toString();
+
+    var format = 'dddd, MMMM Do YYYY, h:mma';
+
+    //$log.info('B DateCtrl: modified: ',date.toString());
+
+    var eventDateInUtc = moment.tz(date, 'Europe/London').format(format);
+
+    $scope.date = eventDateInUtc;
+
+    // TODO the offset seems to be opposite!
+
+}]);
+
